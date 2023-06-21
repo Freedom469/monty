d09c5e3ff90abd95703573aa6ff858f89da7675e
freedom469 <bamjnr99@gmail.com>
2023-06-21 18:55:55 +0300

    checked if the sting was empty

--- main.c
+++ main.c
@@ -1,54 +1,70 @@
 #include "monty.h"
+
 /**
- * main - entry point to the programme
+ * main - entry point to the program
  * @argc: number of arguments
- * @argv: argmunents passed to the programme
+ * @argv: arguments passed to the program
  * Return: zero on success
  */
 int main(int argc, char *argv[])
-{	char *file_name;
+{
+	char *file_name;
 	FILE *file;
 	unsigned int line_number = 0;
 	char line[256];
-	int value;
-	char command[256];
 	stack_t *stack = NULL;
-	char value_str[20];
+	char *opcode;
+	char *argument;
 
 	if (argc != 2)
-	{	fprintf(stderr, "USAGE: monty file\n");
+	{
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	else
-	{	file_name = argv[1];
-		file = fopen(file_name, "r");
-		if (file == NULL)
-		{	fprintf(stderr, "Error: Can't open file %s\n", file_name);
+
+	file_name = argv[1];
+	file = fopen(file_name, "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", file_name);
+		exit(EXIT_FAILURE);
+	}
+
+	while (fgets(line, sizeof(line), file) != NULL)
+	{
+		line_number++;
+
+		opcode = strtok(line, " \t\n");
+		if (opcode == NULL || opcode[0] == '#')
+			continue;
+
+		argument = strtok(NULL, " \t\n");
+
+		if (strcmp(opcode, "push") == 0)
+		{
+			if (argument == NULL)
+			{
+				fprintf(stderr, "Error: L%u: usage: push integer\n", line_number);
+				exit(EXIT_FAILURE);
+			}
+			push(&stack, line_number, argument);
+		}
+		else if (strcmp(opcode, "pall") == 0)
+		{
+			pall(&stack);
+		}
+		else if (strcmp(opcode, "pint") == 0)
+		{
+			pint(&stack);
+		}
+		else
+		{
+			fprintf(stderr, "Error: L%u: unknown instruction %s\n", line_number, opcode);
 			exit(EXIT_FAILURE);
 		}
-		while (fgets(line, sizeof(line), file) != NULL)
-		{	line_number++;
-			if (sscanf(line, "%s %d", command, &value) >= 1)
-			{
-				if (strcmp(command, "push") == 0)
-				{	sprintf(value_str, "%d", value);
-					push(&stack, line_number, value_str);
-				}
-				else if (strcmp(command, "pall") == 0)
-				{	pall(&stack);
-				}
-				else if (strcmp(command, "pint") == 0)
-				{
-					pint(&stack);
-				}
-				else
-				{	fprintf(stderr, "Error: L%u: unknown instruction %s\n",
-						line_number, command);
-				exit(EXIT_FAILURE);
-				}
-			}
-		}
-		fclose(file);
 	}
-	return (0);
+
+	fclose(file);
+	return 0;
 }
+
